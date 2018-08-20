caf6e9a446fba7ff2c0eb60809280995944d9289
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-20 14:01:25 -0600

    added assets

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,7 +18,7 @@ import './about.css';
               <p>Water's like me. It's laaazy ... Boy, it always looks for the easiest way to do things Let's make some happy little clouds in our world. This is a happy place, little squirrels live here and play. We'll do another happy little painting. A fan brush is a fantastic piece of equipment. Use it. Make friends with it. We might as well make some Almighty mountains today as well, what the heck.</p>
             </Col>
             <Col xs={12} sm={4} className="sidebar-section">
-              <Image src="assets/shaun2.jpg" rounded />
+              <Image src="./assets/shaun2.jpg" rounded />
               <p>I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff. The secret to doing anything is believing that you can do it. Anything that you believe you can do strong enough, you can do. Anything. As long as you believe. Just go out and talk to a tree. Make friends with it. I guess that would be considered a UFO. A big cotton ball in the sky. Tree trunks grow however makes them happy. In nature, dead trees are just as normal as live trees.</p>
             </Col>
           </Row>
