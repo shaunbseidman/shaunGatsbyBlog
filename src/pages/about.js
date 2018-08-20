19f73fc348f10da4c425293f2f5932f057323bff
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-20 15:12:07 -0600

    trying to get photo to show

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,10 +9,10 @@ const AboutPage = (props) => {
       <h2>About</h2>
       <Row>
         <Col xs={12} sm={4} className="sidebar-section">
-          <Image src="assets/shaun2.jpg" rounded="rounded"/>
+          <Image src="../../public/assets/shaun2.jpg" rounded="rounded"/>
           <p>I'm Shaun. I am a junior developer based in Denver, Colorado.</p>
           <p>I have recently graduated from Galvanize's six month Web Development Immersive course. The intent with creating this blogspace in addition to my personal website,
-            <a href="http://www.shaunseidman.com">shaunseidman.com</a>
+             <a href="http://www.shaunseidman.com">shaunseidman.com</a>
             was to have a living breathing blog where I can document my struggles finding a job.</p>
           <p>This blog was made using React and Gatsby, two technologies that are somewhat new to me</p>
         </Col>
