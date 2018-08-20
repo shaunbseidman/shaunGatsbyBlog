f822924bd2c850a2f4d4bef4aebac35bc4d96ab1
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-20 13:27:43 -0600

    css extension

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         path: `${__dirname}/src/pages`,
         name:'pages',
-        extensions: [ '', '.js', '.jsx' ],
+        extensions: [ '', '.js', '.jsx','css' ],
       }
     },
     'gatsby-transformer-remark'
