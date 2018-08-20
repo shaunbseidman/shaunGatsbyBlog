58920d133e2bee0130dfcdf30507f012f229e3b6
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-20 13:40:22 -0600

    shit

--- src/components/CustomNavBar.js
+++ src/components/CustomNavBar.js
@@ -3,38 +3,37 @@ import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import './customnavbar.css'
 import Link from 'gatsby-link'
 
-
-export default class CustomNavbar extends Component {
-  render() {
-    return (
-      <Navbar staticTop default collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-        </Navbar.Brand>
-        <Navbar.Toggle/>
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} componentClass={Link} href='/' to='/'>
-            Home
-          </NavItem>
-          <NavItem eventKey={2} componentClass={Link} href='/' to='/about'>
-            About
-          </NavItem>
-          <NavItem eventKey={2} componentClass={Link} href='/' to='/blog'>
-            Blog
-          </NavItem>
-        </Nav>
-        <Nav pullRight>
-          <NavItem eventKey={1} href="http://www.shaunseidman.com">
-            My Personal Website
-          </NavItem>
-          <NavItem eventKey={2} href="https://github.com/shaunbseidman">
-            GitHub
-          </NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
-  }
+const CustomNavbar  = (props) => {
+  return (
+    <Navbar staticTop default collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+      </Navbar.Brand>
+      <Navbar.Toggle/>
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} componentClass={Link} href='/' to='/'>
+          Home
+        </NavItem>
+        <NavItem eventKey={2} componentClass={Link} href='/' to='/about'>
+          About
+        </NavItem>
+        <NavItem eventKey={2} componentClass={Link} href='/' to='/blog'>
+          Blog
+        </NavItem>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="http://www.shaunseidman.com">
+          My Personal Website
+        </NavItem>
+        <NavItem eventKey={2} href="https://github.com/shaunbseidman">
+          GitHub
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  );
 }
+
+export default CustomNavbar
