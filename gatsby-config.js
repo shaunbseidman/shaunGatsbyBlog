dcb57694b8f86566d262bab256f74bb8ba5dac52
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-20 13:31:59 -0600

    fuck

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
       options: {
         path: `${__dirname}/src/pages`,
         name:'pages',
-        extensions: [ '', '.js', '.jsx','css' ],
       }
     },
     'gatsby-transformer-remark'
