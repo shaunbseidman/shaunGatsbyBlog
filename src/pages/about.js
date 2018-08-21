627d27b3ab0145f492772cd0e67c6c4a2290e86c
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-21 11:36:13 -0600

    added to about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,41 +2,49 @@ import React from 'react';
 import {Grid, Row, Col, Image} from 'react-bootstrap';
 import shaun from './images/shaun2.jpg'
 
-
 import './about.css';
 
 const AboutPage = (props) => {
-  return (
-    <div className = 'aboutInfo'>
-      <h2>About</h2>
-      <Row>
-        <Col xs={12} sm={4} className="sidebar-section">
-          <Image src = {shaun}  rounded/>
-          <p>I'm Shaun. I am a junior developer based in Denver, Colorado.</p>
-          <p>I have recently graduated from Galvanize's six month Web Development Immersive course. The intent with creating this blogspace in addition to my personal website, <a href="http://www.shaunseidman.com">shaunseidman.com</a> was to have a living breathing blog where I can document my struggles finding a job.</p>
-          <p>This blog was made using React and Gatsby, two technologies that are somewhat new to me</p>
-        </Col>
-        <Col xs={12} sm={8} className="main-section">
-          <p>My current stack includes and is not limited to:
-            <ul>
-              <li>React</li>
-              <li>Vue.Js</li>
-              <li>JavaScript</li>
-              <li>CSS</li>
-              <li>HTML</li>
-              <li>Express</li>
-              <li>Knex</li>
-              <li>Cypress</li>
-              <li>Git</li>
-              <li>Python</li>
-              <li>Agile</li>
-              <li>Swift</li>
-              <li>Node.Js</li>
-              <li>PostgreSQL</li>
-            </ul>
-          </p>
-        </Col>
-      </Row>
+  return (<div className='aboutInfo'>
+    <h2>About</h2>
+    <Row>
+      <Col xs={6} sm={4} className="sidebar-section">
+        <Image src={shaun} rounded="rounded"/>
+        <p>I'm Shaun. I am a junior developer based in Denver, Colorado.</p>
+        <p>I have recently graduated from Galvanize's six month Web Development Immersive course. The intent with creating this blogspace in addition to my personal website,
+          <a href="http://www.shaunseidman.com">shaunseidman.com</a>
+          was to have a living breathing blog where I can document my struggles finding a job.</p>
+        <p>This blog was made using React and Gatsby, two technologies that are somewhat new to me</p>
+      </Col>
+      <Col xs={6} sm={4} className="main-section">
+        <p>My current stack includes and is not limited to:    </p>
+          <ul>
+            <li>React</li>
+            <li>Vue.Js</li>
+            <li>JavaScript</li>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>Express</li>
+            <li>Knex</li>
+            <li>Cypress</li>
+            <li>Git</li>
+            <li>Python</li>
+            <li>Agile</li>
+            <li>Swift</li>
+            <li>Node.Js</li>
+            <li>PostgreSQL</li>
+          </ul>
+
+      </Col>
+      <Col xs={6} sm={4} className="main-section">
+        <p>Feel free to contact me at:</p>
+          <ul>
+            <li>949.599.4271</li>
+            <li>shaunseidman@gmail.com</li>
+            <li>shaunseidman.com</li>
+          </ul>
+      </Col>
+    </Row>
   </div>);
 }
 export default AboutPage
