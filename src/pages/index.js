17235a75385dac4bdadf54a1fcb3a4eb0be9a36c
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-22 10:36:23 -0600

    added typejs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './index.css';
 import Anime from 'react-anime';
+import Typed from 'react-typed';
+
 import Particles from 'react-particles-js';
-
-
 import {
   Jumbotron,
   Grid,
@@ -14,35 +14,14 @@ import {
   Button
 } from 'react-bootstrap'
 
-// <Particles className='particles' params={{
-//     particles: {
-//       line_linked: {
-//         shadow: {
-//           enable: true,
-//           color: "red",
-//           blur: 5
-//         }
-//       }
-//     }
-//   }} style={{
-//     backgroundColor: 'green',
-//     position: 'absolute',
-//     width: '100%',
-//     height: '100&',
-//     zIndex: '99999',
-//   }}/>
-
 const IndexPage = () => (<div>
 
-
-
-
-  <Jumbotron>
-    <h1 className='indexh1'>Welcome to my blog</h1>
-    <p className='indexP'>Lessons learned from a junior dev</p>
-  </Jumbotron>
-
-
+  <Typed
+    style={{
+    }}
+    strings={['Welcome to my blog']} typeSpeed={40}/>
+  <br/>
+    <Typed strings={['Lessons learned from a junior developer']} typeSpeed={90}/>
 
 </div>)
 
